1e6845fb4caa2b220a15580618aa6750af4b71fe
YinAnPing <yapex@192.168.5.4>
2026-03-31 19:04:26 +0800

    feat: add inventory turnover calculator (存货周转率)
    存货周转率 = 营业成本 / 平均存货
    支持 A/HK/US 三个市场

---
+++ value-investment/calculators/calc_inventory_turnover.py
@@ -0,0 +1,12 @@
+"""存货周转率计算器 = 营业成本 / 平均存货"""
+SUPPORTED_MARKETS = ["A", "HK", "US"]
+REQUIRED_FIELDS = ["operating_cost", "inventory"]
+
+def calculate(data, config):
+    import pandas as pd
+    cost = data["operating_cost"]
+    inv = data["inventory"]
+    # 平均存货 = (本期存货 + 上期存货) / 2，首年无上期则用当年存货
+    avg_inv = (inv + inv.shift(1)) / 2
+    avg_inv.iloc[0] = inv.iloc[0]
+    return cost / avg_inv.replace(0, float('nan'))
